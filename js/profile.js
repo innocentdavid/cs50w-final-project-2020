d527b3ccdb1ac72383adfd76f09f98a198958dc3
innocent <paulinnocent04@gmail.com>
2020-08-03 21:40:42 +0100

    'update'

---
+++ js/profile.js
@@ -0,0 +1,11 @@
+function addSrc() {
+    $('.add-exp-box').css('display','none');
+    $('.add-src-box').css('display','block');
+}
+function addExp() {
+    $('.add-src-box').css('display','none');
+    $('.add-exp-box').css('display','block');
+}
+$(document).ready(function () {
+    
+})
